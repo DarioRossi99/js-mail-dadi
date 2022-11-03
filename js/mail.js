1051c5dba7f41b61ed341f9d708af9192441efb1
DarioRossi99 <rssdra99@gmail.com>
2022-11-03 16:53:41 +0100

    riparazione codice

--- js/mail.js
+++ js/mail.js
@@ -2,15 +2,23 @@ const dati = prompt ("inserire email Valida")
 
 const email = ["roberto@email.com" , "giulia@email.com" , "antonio@email.com" , "giorgia@email.com" , ] 
 
+console.log (email)
+
 let Giusta = false;
 
 for ( let i = 0; i < email.length; i++) {
-
     if ( email[ i ] === dati){
         Giusta = true;
-        console.log ("email corretta")
-    }else ( email[ i ] === ""){
-        Giusta = true;
-        console.log ("email errata")
+        
+    }else {
+       
     }
+}
+
+if ( Giusta === true){
+    Giusta = true;
+    alert ("email corretta")
+}else {
+    Giusta = true;
+    alert ("email errata")
 }
